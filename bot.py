9a7b2398ed0b590b2c1b7869c96cbaf6315b25b4
leofallfall <leonardo.falleroni@gmail.com>
2025-11-25 17:57:50 +0100

    ssss

--- bot.py
+++ bot.py
@@ -1,14 +1,15 @@
 import os
 import time
 import requests
-import telegram
+import asyncio
+from telegram import Bot
 
 TOKEN = os.environ.get("BOT_TOKEN")
 CHAT_ID = int(os.environ.get("CHAT_ID"))
 
-bot = telegram.Bot(token=TOKEN)
+bot = Bot(token=TOKEN)
 
-def check_availability():
+async def check_availability():
     url = "https://booking.resdiary.com/api/Restaurant/TRATTORIATRIPPA/AvailabilityForDateRange"
     payload = {
         "DateFrom": "2025-10-20T00:00:00",
@@ -21,23 +22,23 @@ def check_availability():
 
     r = requests.post(url, json=payload)
     data = r.json()
-
     available = data.get("AvailableDates", [])
 
     if available:
-        bot.send_message(chat_id=CHAT_ID, text=f"🎉 Tavolo trovato!\n\n{available}")
+        print("DEBUG: ready to send message")
+        await bot.send_message(chat_id=CHAT_ID, text=f"🎉 Tavolo trovato!\n\n{available}")
     else:
+        await bot.send_message(chat_id=CHAT_ID, text=f"🎉 Tavolo trovato!\n\n{available}")
+        
         print("Nessun tavolo trovato.")
-        bot.send_message(chat_id=CHAT_ID, text=f"🎉 Tavolo trovato!\n\n{available}")
 
-def loop():
+async def loop():
     while True:
         try:
-            check_availability()
+            await check_availability()
         except Exception as e:
             print("Errore:", e)
-
-        time.sleep(300)  # 5 minuti
+        await asyncio.sleep(300)  # 5 minuti
 
 if __name__ == "__main__":
-    loop()
+    asyncio.run(loop())
