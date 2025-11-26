aff63a1c75d3f38a09e55d996f43606a992d9421
leofallfall <leonardo.falleroni@gmail.com>
2025-11-26 09:56:19 +0100

    nuovodeploy

--- bot.py
+++ bot.py
@@ -1,11 +1,10 @@
 import os
-import time
-import requests
 import asyncio
+import requests
 from telegram import Bot
 
-TOKEN = os.environ.get("BOT_TOKEN")
-CHAT_ID = int(os.environ.get("CHAT_ID"))
+TOKEN = os.environ["BOT_TOKEN"]
+CHAT_ID = os.environ["CHAT_ID"]
 
 bot = Bot(token=TOKEN)
 
@@ -22,12 +21,16 @@ async def check_availability():
 
     r = requests.post(url, json=payload)
     data = r.json()
+
     available = data.get("AvailableDates", [])
 
     if available:
-        print("DEBUG: ready to send message")
-        await bot.send_message(chat_id=CHAT_ID, text=f"🎉 PorcaVaccaaa s'è liberato un tavolooo corriii!\n\n{available}")
-    else:        
+        await bot.send_message(
+            chat_id=CHAT_ID,
+            text=f"🎉 Tavolo trovato!\n\n{available}"
+        )
+        print("Messaggio inviato!")
+    else:
         print("Nessun tavolo trovato.")
 
 async def loop():
@@ -36,7 +39,8 @@ async def loop():
             await check_availability()
         except Exception as e:
             print("Errore:", e)
-        await asyncio.sleep(300)  # 5 minuti
+
+        await asyncio.sleep(300)
 
 if __name__ == "__main__":
     asyncio.run(loop())
